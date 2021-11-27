85505c9ef8716ab14dfb321d5e3b3b388a66677d
JeanMarko173 <jeanmarko.13.7@gmail.com>
2021-11-27 10:20:33 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Row, Col, Carousel, Card, Space } from 'antd';
+import { useState, useEffect } from 'react'
+const { Meta } = Card
 
 function App() {
+
+  const [featuredServices, setFeaturedServices] = useState([1,2,3,4]);
+  const [products, setProducts] = useState([1,2,3,4]);
+
+  useEffect( ()=> {
+    //Aca debemos consumir los servicios y setear los estados
+  },[])
+
+  const contentStyle = {
+    height: '300px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row>
+        <Col span={24}>
+          <Carousel autoplay>
+          <Space size={8}>
+            <img alt="example" src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" style={{'max-width': '100px', height: '100px', 'objet-fit': 'cover'}} />
+          </Space>
+          <Space>
+            <img alt="example" src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" style={{'max-width': '100px', height: '100px', 'objet-fit': 'cover'}} />
+          </Space>
+          <Space>
+            <img alt="example" src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" style={{'max-width': '100px', height: '100px', 'objet-fit': 'cover'}} />
+          </Space>
+          <Space>
+            <img alt="example" src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" style={{'max-width': '100px', height: '100px', 'objet-fit': 'cover'}} />
+          </Space>
+          </Carousel>
+        </Col>
+      </Row>
+      <Row>
+        {products.map( item => (
+          <Card
+            key={item}
+            hoverable
+            style={{ width: 200 }}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+          >
+            <Meta title="Europe Street beat" description="www.instagram.com" />
+          </Card>
+        ))}
+      </Row>
+    </>
   );
 }
 
