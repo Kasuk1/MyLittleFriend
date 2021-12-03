a2f8567dfc3a4d513ea2880d917c3686a5b5044d
Andres <andrestheran@outlook.com>
2021-12-02 20:10:43 -0500

    cambios en Appjs

--- src/components/globales/footer/Footer.js
+++ src/components/globales/footer/Footer.js
@@ -1,7 +1,24 @@
-export const Footer = () => {
-    return (
-        <div>
+import { UserOutlined, FacebookOutlined, InstagramOutlined,PhoneOutlined } from '@ant-design/icons';
+import imagotipo from './assets/imagotipo.png'
 
+const { Footer } = Layout;
+
+export const Footer = () => {
+  return (
+    <Footer id={Footer} style={{ padding: "0 0" }}>
+      <div className="contact">
+        <img
+          classname="imagotipo"
+          src={imagotipo}
+          alt="imagotipo"
+          style={{ height: "100px", objectFit: "cover" }}
+        />
+        <div className="redes">
+          <FacebookOutlined />
+          <InstagramOutlined />
+          <PhoneOutlined />
         </div>
-    )
-}
+      </div>
+    </Footer>
+  );
+};
