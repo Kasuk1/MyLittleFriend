601691a50e35143a3f0ff83c25a3e77b0369e139
Andres <andrestheran@outlook.com>
2021-12-02 20:57:25 -0500

    cambios en footer y navbar

--- src/components/globales/footer/Footer.js
+++ src/components/globales/footer/Footer.js
@@ -1,9 +1,11 @@
-import { UserOutlined, FacebookOutlined, InstagramOutlined,PhoneOutlined } from '@ant-design/icons';
-import imagotipo from './assets/imagotipo.png'
+import { FacebookOutlined, InstagramOutlined,PhoneOutlined } from '@ant-design/icons';
+import imagotipo from '../../../assets/imagotipo.png';
+import { Layout } from 'antd';
+
 
 const { Footer } = Layout;
 
-export const Footer = () => {
+export const FooterComponent = () => {
   return (
     <Footer id={Footer} style={{ padding: "0 0" }}>
       <div className="contact">
@@ -11,7 +13,7 @@ export const Footer = () => {
           classname="imagotipo"
           src={imagotipo}
           alt="imagotipo"
-          style={{ height: "100px", objectFit: "cover" }}
+          style={{ height: "70px", objectFit: "cover" }}
         />
         <div className="redes">
           <FacebookOutlined />
