96892f2ba880c9a00e9fbca5e4a99bae18b1263c
marcela <marceg8810@gmail.com>
2021-12-03 08:34:23 -0500

    Solicitar servicio

--- src/components/servicios/solicitudServicio/ServicioSolicitud.js
+++ src/components/servicios/solicitudServicio/ServicioSolicitud.js
@@ -1,121 +1,115 @@
 
 import {  } from '@ant-design/colors';
 import 'antd/dist/antd.css';
-import {
-    Row,
-    Col,
-    Form,
-    Input,
-    Button,
-    Radio,
-    Select,
-    Cascader,
-    DatePicker,
-    InputNumber,
-    TreeSelect,
-    Switch,
-    Typography,
-    
-  } from 'antd';
+import {Form, Row, Col, Select, Input, DatePicker, Button, Typography, Space} from 'antd'
 import { useState } from 'react/cjs/react.development';
-  
-const {Title} = Typography
+import locale from 'antd/es/date-picker/locale/es_ES';
+import './ServicioSolicitud.css'
+
+const {Item}= Form
+const {Password}= Input
+const {Title}= Typography
+
 export const ServicioSolicitud = () => {
-  const uploadButton = (
-    <div>
-        {loading ? <LoadingOutlined /> : <PlusOutlined />}
-        <div style={{ marginTop: 8 }}>Upload</div>
-    </div>
-);
-  const tailFormItemLayout = {
-    wrapperCol: {
-        xs: {
-            span: 24,
-            offset: 0,
-        },
-        sm: {
-            span: 16,
-            offset: 8,
-        },
+
+  const formSuccess = (datos) =>{
+    console.log("formulario enviado", datos);
+  } 
+  const formFailed = (error) =>{
+    console.log("error", error);
+  } 
+  const formItemLayout = {
+    labelCol: {
+      xs:{
+        span: 12,
       },
-    };   
-    
-    return (
-      <Row align="center" className="formulario-registro">
-        <Col lg={6}>
-        <Form
-        name="basic"
-        labelCol={{
-            span: 8,
-        }}
-        wrapperCol={{
-            span: 48,
-        }}
-        initialValues={{
-            remember: true,
-        }}
-        // ref={formRef}
-        // {...formItemLayout}
-        // onFinish={onFinish}
-        // onFinishFailed={onFinishFailed}
-        // autoComplete="off"
-        >
-        <Title level={2}>Reservar un servicio</Title>
-        <Form.Item 
-          label="Seleccione servicio"
-          name="servicio"
-          rules={[
+      sm:{
+        span:8,
+      },
+    },
+    WrapperCol: {
+      xs: {
+        span: 4,
+      },
+      sm: {
+        span:20,
+      },
+    },
+  }  
+
+  return (
+    <div className="box">
+      <Row align="middle">
+       <Col xs={1} sm={2} md={6} lg={8} ></Col>
+       <Col xs={22} sm={20} md={12} lg={8} >
+       
+         <Form name="formulario" 
+          onFinish={formSuccess}
+          onFinishFailed={formFailed}
+          {...formItemLayout}
+         >
+           
+           <Item>
+            <Title level={2}>Reservar un servicio</Title>
+           </Item>
+           <Item label="Seleccione servicio"
+            name="servicio"
+            rules={[
             {
                 required: true,
                 message: 'Porfavor, escoja un servicio!',
             },
-          ]}
-        >
-          <Select placeholder="Selecciona un servicio">
-            <Select.Option value="1">Consulta médica</Select.Option>
-            <Select.Option value="2">Limpieza mascota</Select.Option>
-            <Select.Option value="3">Corte y masajes</Select.Option>
-          </Select>
-        </Form.Item>
-        <Form.Item 
-          label="Tipo de mascota"
-          name="tipo"
-          rules={[
+            ]}
+            >
+            <Select placeholder="Selecciona un servicio">
+              <Select.Option value="1">Consulta médica</Select.Option>
+              <Select.Option value="2">Limpieza mascota</Select.Option>
+              <Select.Option value="3">Corte y masajes</Select.Option>
+            </Select>
+           </Item>
+           <Item label="Tipo de mascota"
+            name="tipo"
+            rules={[
             {
                 required: true,
                 message: 'Porfavor, seleccione tipo de mascota!',
             },
-          ]}
-        >
-          <Select placeholder="Tipo de mascota">
-            <Select.Option value="1">Perro</Select.Option>
-            <Select.Option value="2">Gato</Select.Option>
-            <Select.Option value="3">Hamster</Select.Option>
-          </Select>
-        </Form.Item>
-      <Form.Item 
-        label="Nombre de la mascota">
-          <Input />
-      </Form.Item>
-      <Form.Item 
-        label="Seleccione la fecha"
-        name="fechacita"
-        rules={[
-          {
+            ]}
+            >
+            <Select placeholder="Tipo de mascota">
+              <Select.Option value="1">Perro</Select.Option>
+              <Select.Option value="2">Gato</Select.Option>
+              <Select.Option value="3">Hamster</Select.Option>
+            </Select>
+          </Item>
+          <Item 
+            label="Nombre de la mascota">
+            <Input />
+          </Item>
+          <Item 
+            label="Seleccione la fecha"
+            name="fechacita"
+            rules={[
+            {
               required: true,
               message: 'Porfavor, selecciona una fecha!',
-          }
-        ]}
-        {...tailFormItemLayout}
-      >
-        <DatePicker  />
-      </Form.Item>
-      <Form.Item label="     ">
-        <Button type="primary">Reservar</Button>
-      </Form.Item>
-    </Form>   
-        </Col>
-      </Row>
+            }
+            ]}
+          >
+            <DatePicker placeholder="Seleccione la fecha de la cita" style={{width: "100%"}} locale={locale} />
+          </Item>
+          <Item style={{textAlign: "center"}} >
+            <Button type="primary">Reservar</Button>
+          </Item>
+         </Form>
+       </Col>
+       <Col xs={1} sm={2} md={6} lg={8} ></Col>
+     </Row>
+    </div>
+       
+    
+     
+       
         
     )
 }
