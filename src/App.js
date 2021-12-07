94a71a6ba391dfc026a6676e48c08952970ccc10
marcela <marceg8810@gmail.com>
2021-12-06 20:39:02 -0500

    Cambios marcela

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { Typography } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
 
-import { ServicioSolicitud } from './components/servicios/solicitudServicio/ServicioSolicitud';
-import { ListaServicios } from './components/servicios/listaServicio/ListaServicios.js';
-
-
-const{Title}= Typography;
 
 function App() {
   return (
     <div className="App">
-      {/* <ServicioSolicitud/> */}
-      <ListaServicios/>
-      
+           
     </div>
   );
 }
